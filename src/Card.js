d3761398d79f45b42eaa385dee2c06b58f4e20d8
Adam <cloudflips321@icloud.com>
2022-09-04 20:57:07 -0400

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './Card.css';
 
 export default function Card(props) {
+  
   return (
     <div className='Card'>
-      <button type='button' onClick={() => this.props.onDelete(this.props.id)}>
+      <button type='button' onClick={() => this.props.filter(this.props.id)}>
         delete
       </button>
       <h3>{props.title}</h3>
